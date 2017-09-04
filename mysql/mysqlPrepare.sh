a811b8f3d758664c8862f4195668d9b279910f7d
timmybyster <tim@aece.co.za>
2017-09-04 08:45:10 +0200

    All configurations for the MySQL and hwClock. hwclock is configured with a new rc.local file. MySQL DB is cleared of all logs, warnings and any node that is not the IBC/CCB

---
+++ mysql/mysqlPrepare.sh
@@ -0,0 +1,6 @@
+mysql -u root -proot ibspiui -se "DELETE FROM nodes WHERE type_id != 0"
+mysql -u root -proot ibspiui -se "DELETE FROM logs"
+mysql -u root -proot ibspiui -se "DELETE FROM warnings"
+mysql -u root -proot ibspiui -se "DELETE FROM incoming_packets"
+serial=$(sed -n '1 p' Install/install)
+mysql -u root -proot ibspiui -se "UPDATE nodes SET serial = $serial"
